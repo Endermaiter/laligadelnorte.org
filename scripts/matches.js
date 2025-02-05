017dfec2dfb9aac98247fcb1e6c9e4ccb43ba70f
Endermaiter <marcospnovoa@gmail.com>
2025-02-05 19:49:38 +0100

    Add new matches

---
+++ scripts/matches.js
@@ -0,0 +1,10 @@
+
+function navigate(index) {
+    const slider = document.querySelector('.container-matches').getElementsByClassName("match-section"); // Aseguramos que selecciona el contenedor correcto
+    console.log(slider.length)
+    for (let i=0;i < slider.length; i++){
+        if (i==index){
+            
+        }
+    }
+}
