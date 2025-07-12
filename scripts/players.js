0b646cf23f4f7021d983833de0aa9bd44caef64e
Endermaiter <marcospnovoa@gmail.com>
2025-07-12 23:13:24 +0200

    Final commit before release?

--- scripts/players.js
+++ scripts/players.js
@@ -9,22 +9,23 @@ function renderPlayers(list) {
     const card = document.createElement("div");
     const imgSrc = `../src/rank/${player.rank}.png`;
     const teamSrc = `../src/teams_logo/${player.team}.png`;
+    const opgg_url = `https://op.gg/es/lol/summoners/euw/${player.name.replace(" ", "%20").replace("#", "-")}`;
     card.className = "player-card";
     card.innerHTML = `
       <h3 class="player-name">${player.name}</h3>
 
       <div class="info-row">
-        <div class="info-block">
+        <div class="info-block-1">
           <img class="rank-icon" src="${imgSrc}" alt="${player.rank}" />
           <p class="label">${player.rank}</p>
         </div>
-        <div class="info-block">
+        <div class="info-block-2">
           <img class="team-logo" src="${teamSrc}" alt="${player.team}" />
           <p class="label">${player.team}</p>
         </div>
       </div>
     `;
-    card.onclick = () => window.open(player.opgg, "_blank");
+    card.onclick = () => window.open(opgg_url, "_blank");
     grid.appendChild(card);
   });
 }
@@ -39,6 +40,12 @@ fetch("../data/players.json")
 
 search.addEventListener("input", () => {
   const query = search.value.toLowerCase();
-  const filtered = players.filter(p => p.name.toLowerCase().includes(query));
+
+  const filtered = players.filter(p =>
+    p.name.toLowerCase().includes(query) ||
+    p.team.toLowerCase().includes(query) ||
+    p.rank.toLowerCase().includes(query)
+  );
+
   renderPlayers(filtered);
 });
