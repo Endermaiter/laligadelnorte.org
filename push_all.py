0c28792e055c23c5f9479e0fb705cb37eafd611f
Endermaiter <marcospnovoa@gmail.com>
2025-10-05 15:19:30 +0200

    Auto push 05/10/2025 - 15:19

--- push_all.py
+++ push_all.py
@@ -7,3 +7,7 @@ current_date= datetime.now().strftime("%d/%m/%Y - %H:%M")
 subprocess.run("git add *", shell=True)
 subprocess.run(f'git commit -m "Auto push {current_date}"')
 subprocess.run("git push origin master", shell=True)
+
+print("\n\n ")
+print("\n\n✅ | Commit pushed!")
+print("\n\n ")
