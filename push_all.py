3b4fd1e55cc71162566b2151153c9b5a4aae8101
Endermaiter <marcospnovoa@gmail.com>
2025-11-21 00:58:15 +0100

    Auto push 21/11/2025 - 00:58

--- push_all.py
+++ push_all.py
@@ -7,6 +7,9 @@ current_date= datetime.now().strftime("%d/%m/%Y - %H:%M")
 subprocess.run("git add *", shell=True)
 subprocess.run(f'git commit -m "Auto push {current_date}"', shell=True)
 current_branch = subprocess.run("git branch --show-current", shell=True, capture_output=True, text=True)
-subprocess.run(f"git push origin {current_branch}", shell=True)
+push_output = subprocess.run(f"git push origin {current_branch}", shell=True)
 
-print("\n✅ | Commit pushed!\n")
+if push_output.returncode == 0:
+    print("\n✅ | Push succeed!\n")
+else:
+    print("\n❌ | Push failed!\n")
